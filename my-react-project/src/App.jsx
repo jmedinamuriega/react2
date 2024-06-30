c9c4d016bdef2d6a4d35564e5712fc2e00f0b3f1
JuanMedina <jmedinamuriega@gmail.com>
2024-06-30 19:39:04 -0300

    First commit

---
+++ my-react-project/src/App.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import './Header.css';
+
+const Header = () => {
+  return (
+    <div className="header">
+      <div className="overlay">
+        <h1>Your Name</h1>
+      </div>
+    </div>
+  );
+};
+
+export default Header;
